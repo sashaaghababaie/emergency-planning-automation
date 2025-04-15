b0472a4df2d1084cd7fadf077f842acf6f4e35a5
sashaaghababaie <sasha.aghababaie@gmail.com>
2025-04-15 21:32:01 +0330

    bump version

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
+  basePath: "/emergency-planning-automation",
+  assetPrefix: "/emergency-planning-automation/",
   experimental: {
     serverActions: {
       bodySizeLimit: "20000mb",
